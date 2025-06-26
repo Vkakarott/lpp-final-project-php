f3e3c1fdd7b27f0949f9959ece9165e953586b4e
Vkakarott <vecctor.vitor15@gmail.com>
2025-06-26 09:52:36 -0300

    feat: pure html

---
+++ create.php
@@ -0,0 +1,5 @@
+<form action="insert.php" method="POST">
+    <input name="title" placeholder="Title" required><br>
+    <textarea name="content" placeholder="Content" required></textarea><br>
+    <button type="submit">Publish</button>
+</form>
