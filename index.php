9bd6d6399304375de40d37bcd156a443e9b1cad6
Vkakarott <vecctor.vitor15@gmail.com>
2025-06-26 09:51:36 -0300

    feat: page posts

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require 'db.php';
+$posts = $pdo->query("SELECT * FROM post ORDER BY created_at DESC")->fetchAll();
+?>
+<h1>Mini Blog</h1>
+<a href="create.php">New Post</a>
+<ul>
+    <?php foreach ($posts as $post): ?>
+        <li>
+            <a href="post.php?id=<?= $post['id'] ?>">
+                <?= htmlspecialchars($post['title']) ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
