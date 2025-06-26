3c917d81a566e9a00199a450ac62ca66ee30103b
Vkakarott <vecctor.vitor15@gmail.com>
2025-06-26 09:54:42 -0300

    feat: add post insertion logic with PDO

---
+++ insert.php
@@ -0,0 +1,7 @@
+<?php
+require 'db.php';
+$title = $_POST['title'];
+$content = $_POST['content'];
+$stmt = $pdo->prepare("INSERT INTO posts (title, content) VALUES (?, ?)");
+$stmt->execute([$title, $content]);
+header("Location: index.php");
