fad2fb917b06c56b1221215364597695f4d69cc8
Vkakarott <vecctor.vitor15@gmail.com>
2025-06-27 10:43:22 -0300

    fix: replace type db

--- cli_insert.php
+++ cli_insert.php
@@ -9,9 +9,12 @@ if (!preg_match('/^[\p{L}\p{N}\s]+$/u', $title)) {
     die("Título inválido.\n");
 }
 
-$db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-$stmt = $db->prepare("INSERT INTO posts (title, content, created_at) VALUES (?, ?, NOW())");
+$db = new PDO('sqlite:data/blog.db');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stmt = $db->prepare("INSERT INTO posts (title, content, created_at) VALUES (?, ?, datetime('now'))");
 $stmt->execute([$title, $content]);
+
 $file = new SplFileObject('backup_posts.txt', 'a');
 $file->fwrite("Título: $title\nConteúdo: $content\n---\n");
 
