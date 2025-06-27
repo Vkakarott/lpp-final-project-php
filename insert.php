a1ec979bd85eb509b6ebbeb93b23de594eb6ab44
Vkakarott <vecctor.vitor15@gmail.com>
2025-06-27 10:20:16 -0300

    feat: add title validation and script sanitization to post submission

--- insert.php
+++ insert.php
@@ -1,7 +1,12 @@
 <?php
 require 'db.php';
-$title = $_POST['title'];
-$content = $_POST['content'];
+$title = trim($_POST['title']);
+if (!preg_match('/^[\p{L}\p{N}\s]+$/u', $title)) {
+    die('Título inválido. Use apenas letras, números e espaços.');
+}
+$content = trim($_POST['content']);
+$content = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', '', $content);
 $stmt = $pdo->prepare("INSERT INTO posts (title, content) VALUES (?, ?)");
 $stmt->execute([$title, $content]);
 header("Location: index.php");
+exit;
